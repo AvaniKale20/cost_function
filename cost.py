ac8b94b26d289c4c6e5145ab136eb53760375afd
Avani Kale <avani.kale@thoughtworks.com>
2020-03-04 14:12:51 +0530

    [avani]Add. should plot the value of x and y

---
+++ cost.py
@@ -0,0 +1,12 @@
+import numpy as numpy
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+# original data set
+x = [1, 2, 3]
+print(len(x))
+y = [1, 2.5, 3.5]
+print(len(y))
+plt.scatter(x, y)
+
+plt.show()
